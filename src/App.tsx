2da2b1b8b5c8bab47e998231ec76b653b0164889
Xenonyy <gonda.armand@gmail.com>
2025-05-22 17:45:05 +0200

    modify text, leave this as a template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export const App = () => {
   return (
     <div className="relative min-h-screen min-w-screen bg-violet-300 flex justify-center text-center text-4xl text-white pt-5">
-      Practice projects
+      Hello World!
     </div>
   );
 };
