7e8a49e56bffac5a5d20ab4d1d195fbf834c4d3e
Xenonyy <gonda.armand@gmail.com>
2025-05-25 02:23:00 +0200

    migrate to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+import { HomePage } from './pages/HomePage';
+
 export const App = () => {
-  return (
-    <div className="relative min-h-screen min-w-screen bg-violet-300 flex justify-center text-center text-4xl text-white pt-5">
-      Hello World!
-    </div>
-  );
+  return <HomePage />;
 };
