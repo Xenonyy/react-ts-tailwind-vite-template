25880bc5b4ad063ef23d47e28a43766829902e3f
Xenonyy <gonda.armand@gmail.com>
2025-05-25 02:30:07 +0200

    add some eslint rules and formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import reactX from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -13,19 +16,19 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: ["prettier", {
+    plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      
-    }],
+      'react-x': reactX,
+      'react-dom': reactDom,
+      'eslintPluginPrettier': eslintPluginPrettier,
+    },
     rules: {
       'prettier/prettier': 'error',
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-        
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      ...reactX.configs['recommended-typescript'].rules,
+      ...reactDom.configs.recommended.rules,
     },
-  },
-)
+  }
+);
